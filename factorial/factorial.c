85e3ece3c53990d310111989d9c88b44e440d4e3
Hyun-Jun Sung <hjsung@gmail.com>
2012-08-26 17:21:31 +0900

    .

---
+++ factorial/factorial.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int factorial(int n)
+{
+	int r, i;
+	r = 1;
+	for (i = 2 ; i < n ; i++) {
+		r *= i;
+	}
+	return r;
+}
+
+int factorial2(int n) {
+	if (n == 1)
+		return 1;
+	else
+		return n * factorial2(n - 1);
+}
+
+void main(){
+	printf("%d\n", factorial(3));
+	printf("%d\n", factorial2(3));
+}
